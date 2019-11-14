3c28e84d98ec17a0e2cf0da7be5abb3b571334e1
Denis Smirnov <darthunix@gmail.com>
2019-11-14 21:23:49 +1000

    Add persistent heap changes

--- x5fix.c
+++ x5fix.c
@@ -15,6 +15,82 @@ PG_MODULE_MAGIC;
 
 #define FileSpaceRelationId  5009
 
+typedef struct PersistentFilespaceSharedData
+{
+
+	PersistentFileSysObjSharedData		fileSysObjSharedData;
+
+} PersistentFilespaceSharedData;
+
+PersistentFilespaceSharedData	*persistentFilespaceSharedData = NULL;
+
+
+typedef struct PersistentFilespaceData
+{
+
+	PersistentFileSysObjData		fileSysObjData;
+
+} PersistentFilespaceData;
+
+#define PersistentFilespaceData_StaticInit {PersistentFileSysObjData_StaticInit}
+
+PersistentFilespaceData	persistentFilespaceData = PersistentFilespaceData_StaticInit;
+
+typedef struct ReadTupleForUpdateInfo
+{
+	PersistentFsObjType 		fsObjType;
+	PersistentFileSysObjName 	*fsObjName;
+	ItemPointerData				persistentTid;
+	int64						persistentSerialNum;
+} ReadTupleForUpdateInfo;
+
+#define READTUPLE_FOR_UPDATE_ERRCONTEXT_DECLARE \
+	ErrorContextCallback readTupleForUpdateErrContext; \
+	ReadTupleForUpdateInfo readTupleForUpdateInfo;
+
+#define READTUPLE_FOR_UPDATE_ERRCONTEXT_POP \
+{ \
+	/* Pop the error context stack */ \
+	error_context_stack = readTupleForUpdateErrContext.previous; \
+}
+
+#define READTUPLE_FOR_UPDATE_ERRCONTEXT_PUSHNAME(name, tid, serialNum) \
+{ \
+	readTupleForUpdateInfo.fsObjType = (name)->type; \
+    readTupleForUpdateInfo.fsObjName = (name); \
+	readTupleForUpdateInfo.persistentTid = *(tid); \
+	readTupleForUpdateInfo.persistentSerialNum = serialNum; \
+\
+	/* Setup error traceback support for ereport() */ \
+	readTupleForUpdateErrContext.callback = PersistentFileSysObj_ReadForUpdateErrContext; \
+	readTupleForUpdateErrContext.arg = (void *) &readTupleForUpdateInfo; \
+	readTupleForUpdateErrContext.previous = error_context_stack; \
+	error_context_stack = &readTupleForUpdateErrContext; \
+}
+
+static void
+PersistentFileSysObj_ReadForUpdateErrContext(void *arg)
+{
+	ReadTupleForUpdateInfo *info = (ReadTupleForUpdateInfo*) arg;
+
+	if (info->fsObjName != NULL)
+	{
+		errcontext(
+			 "Reading tuple TID %s for possible update to persistent file-system object %s, persistent serial number " INT64_FORMAT,
+			 ItemPointerToString(&info->persistentTid),
+			 PersistentFileSysObjName_TypeAndObjectName(info->fsObjName),
+			 info->persistentSerialNum);
+	}
+	else
+	{
+		errcontext(
+			 "Reading tuple TID %s for possible update to persistent file-system object type %s, persistent serial number " INT64_FORMAT,
+			 ItemPointerToString(&info->persistentTid),
+			 PersistentFileSysObjName_TypeName(info->fsObjType),
+			 info->persistentSerialNum);
+	}
+}
+
 Datum
 add_entry_gp_persistent_filespace_node(PG_FUNCTION_ARGS);
 
@@ -130,6 +206,272 @@ static void PersistentFilespace_BlankPadCopyLocation(
 	locationBlankPadded[FilespaceLocationBlankPaddedWithNullTermLen - 1] = '\0';
 }
 
+
+static void fix_heap(PersistentFileSysObjName *fsObjName,
+							   ItemPointer persistentTid,
+							   int64 persistentSerialNum,
+							   int16 pridbid,
+							   int16 mirdbid,
+							   void *priloc,
+                               void *mirloc,
+							   bool	flushToXLog)
+{
+	READTUPLE_FOR_UPDATE_ERRCONTEXT_DECLARE;
+
+	PersistentFsObjType				fsObjType;
+
+	PersistentFileSysObjData 		*fileSysObjData;
+	PersistentFileSysObjSharedData 	*fileSysObjSharedData;
+
+	Datum *values;
+
+	HeapTuple tupleCopy;
+
+	PersistentFileSysObjName		actualFsObjName;
+	PersistentFileSysState			state;
+
+	MirroredObjectExistenceState	mirrorExistenceState;
+
+	TransactionId					parentXid;
+	int64							serialNum;
+
+	fsObjType = fsObjName->type;
+
+	PersistentFileSysObj_GetDataPtrs(
+								fsObjType,
+								&fileSysObjData,
+								&fileSysObjSharedData);
+
+	values = (Datum*)palloc(fileSysObjData->storeData.numAttributes * sizeof(Datum));
+
+	READTUPLE_FOR_UPDATE_ERRCONTEXT_PUSHNAME(fsObjName, persistentTid, persistentSerialNum);
+
+	PersistentFileSysObj_ReadTuple(
+							fsObjType,
+							persistentTid,
+							values,
+							&tupleCopy);
+
+	READTUPLE_FOR_UPDATE_ERRCONTEXT_POP;
+
+	if (tupleCopy != NULL)
+	{
+		GpPersistent_GetCommonValues(
+							fsObjType,
+							values,
+							&actualFsObjName,
+							&state,
+							&mirrorExistenceState,
+							&parentXid,
+							&serialNum);
+
+		Datum *newValues;
+		bool *replaces;
+		bool needs_update = false;
+
+		newValues = (Datum*)palloc0(fileSysObjData->storeData.numAttributes *
+									sizeof(Datum));
+		replaces = (bool*)palloc0(fileSysObjData->storeData.numAttributes *
+								  sizeof(bool));
+
+		if (fsObjType == PersistentFsObjType_FilespaceDir)
+		{
+            replaces[1] = true;
+            newValues[1] =  Int16GetDatum(pridbid);
+
+			Assert(strlen((char *)priloc) == FilespaceLocationBlankPaddedWithNullTermLen - 1);
+			replaces[2] = true;
+			newValues[2] = CStringGetTextDatum((char *)priloc);
+
+            replaces[3] = true;
+            newValues[3] =  Int16GetDatum(mirdbid);
+
+			Assert(strlen((char *)mirloc) == FilespaceLocationBlankPaddedWithNullTermLen - 1);
+			replaces[4] = true;
+			newValues[4] = CStringGetTextDatum((char *)priloc);
+
+			needs_update = true;
+		}
+
+		/* only replace the tuple if we've changed it */
+		if (needs_update)
+		{
+			PersistentFileSysObj_ReplaceTuple(fsObjType,
+											  persistentTid,
+											  tupleCopy,
+											  newValues,
+											  replaces,
+											  flushToXLog);
+		}
+		else
+		{
+			heap_freetuple(tupleCopy);
+		}
+
+		pfree(newValues);
+		pfree(replaces);
+	}
+	pfree(values);
+}
+
+static FilespaceDirEntry
+PersistentFilespace_CreateDirUnderLock(
+	Oid			filespaceOid)
+{
+	bool			found;
+
+	FilespaceDirEntry	filespaceDirEntry;
+
+	FilespaceDirEntryKey key;
+
+	Assert(LWLockHeldByMe(FilespaceHashLock));
+
+	if (persistentFilespaceSharedHashTable == NULL)
+		elog(PANIC, "Persistent filespace information shared-memory not setup");
+
+	key.filespaceOid = filespaceOid;
+
+	filespaceDirEntry =
+			(FilespaceDirEntry)
+					hash_search(persistentFilespaceSharedHashTable,
+								(void *) &key,
+								HASH_ENTER_NULL,
+								&found);
+
+	if (filespaceDirEntry == NULL)
+		elog(ERROR, "Out of shared-memory for persistent filespaces");
+
+	filespaceDirEntry->state = 0;
+	filespaceDirEntry->persistentSerialNum = 0;
+	MemSet(&filespaceDirEntry->persistentTid, 0, sizeof(ItemPointerData));
+
+	return filespaceDirEntry;
+}
+
+static bool PersistentFilespace_ScanTupleCallback(
+	ItemPointer 			persistentTid,
+	int64					persistentSerialNum,
+	Datum					*values)
+{
+	Oid		filespaceOid;
+
+	int16	dbId1;
+	char	locationBlankPadded1[FilespaceLocationBlankPaddedWithNullTermLen];
+
+	int16	dbId2;
+	char	locationBlankPadded2[FilespaceLocationBlankPaddedWithNullTermLen];
+
+	PersistentFileSysState	state;
+
+	int64	createMirrorDataLossTrackingSessionNum;
+
+	MirroredObjectExistenceState	mirrorExistenceState;
+
+	int32					reserved;
+	TransactionId			parentXid;
+	int64					serialNum;
+
+	FilespaceDirEntry filespaceDirEntry;
+
+	GpPersistentFilespaceNode_GetValues(
+									values,
+									&filespaceOid,
+									&dbId1,
+									locationBlankPadded1,
+									&dbId2,
+									locationBlankPadded2,
+									&state,
+									&createMirrorDataLossTrackingSessionNum,
+									&mirrorExistenceState,
+									&reserved,
+									&parentXid,
+									&serialNum);
+
+   /*
+	* Normally we would acquire this lock with the WRITE_FILESPACE_HASH_LOCK
+	* macro, however, this particular function can be called during startup.
+	* During startup, which executes in a single threaded context, no
+	* PersistentObjLock exists and we cannot assert that we're holding it.
+	*/
+	LWLockAcquire(FilespaceHashLock, LW_EXCLUSIVE);
+
+	filespaceDirEntry =	PersistentFilespace_CreateDirUnderLock(filespaceOid);
+
+	filespaceDirEntry->dbId1 = dbId1;
+	memcpy(filespaceDirEntry->locationBlankPadded1, locationBlankPadded1, FilespaceLocationBlankPaddedWithNullTermLen);
+
+	filespaceDirEntry->dbId2 = dbId2;
+	memcpy(filespaceDirEntry->locationBlankPadded2, locationBlankPadded2, FilespaceLocationBlankPaddedWithNullTermLen);
+
+	filespaceDirEntry->state = state;
+	filespaceDirEntry->persistentSerialNum = serialNum;
+	filespaceDirEntry->persistentTid = *persistentTid;
+
+	LWLockRelease(FilespaceHashLock);
+
+	if (Debug_persistent_print)
+		elog(Persistent_DebugPrintLevel(),
+			 "PersistentFilespace_ScanTupleCallback: filespace %u, dbId1 %d, dbId2 %d, state '%s', mirror existence state '%s', TID %s, serial number " INT64_FORMAT,
+			 filespaceOid,
+			 dbId1,
+			 dbId2,
+			 PersistentFileSysObjState_Name(state),
+			 MirroredObjectExistenceState_Name(mirrorExistenceState),
+			 ItemPointerToString2(persistentTid),
+			 persistentSerialNum);
+
+	return true;	// Continue.
+}
+
+static Size PersistentFilespace_SharedDataSize(void)
+{
+	return MAXALIGN(sizeof(PersistentFilespaceSharedData));
+}
+
+void PersistentFilespace_ShmemInit(void)
+{
+	bool found;
+	bool ok;
+
+	/* Create the shared-memory structure. */
+	persistentFilespaceSharedData =
+		(PersistentFilespaceSharedData *)
+						ShmemInitStruct("Persistent Filespace Data",
+										PersistentFilespace_SharedDataSize(),
+										&found);
+
+	if (!found)
+	{
+		PersistentFileSysObj_InitShared(
+						&persistentFilespaceSharedData->fileSysObjSharedData);
+	}
+
+	/* Create or find our shared-memory hash table. */
+	ok = PersistentFilespace_HashTableInit();
+	if (!ok)
+		ereport(FATAL,
+				(errcode(ERRCODE_OUT_OF_MEMORY),
+				 errmsg("Not enough shared memory for persistent filespace hash table")));
+
+	PersistentFileSysObj_Init(
+						&persistentFilespaceData.fileSysObjData,
+						&persistentFilespaceSharedData->fileSysObjSharedData,
+						PersistentFsObjType_FilespaceDir,
+						PersistentFilespace_ScanTupleCallback);
+
+
+	Assert(persistentFilespaceSharedData != NULL);
+	Assert(persistentFilespaceSharedHashTable != NULL);
+}
+
+static void PersistentFilespace_VerifyInitScan(void)
+{
+	if (persistentFilespaceSharedData == NULL)
+		elog(PANIC, "Persistent filespace information shared-memory not setup");
+
+	PersistentFileSysObj_VerifyInitScan();
+}
+
 Datum
 add_entry_gp_persistent_filespace_node(PG_FUNCTION_ARGS)
 {
@@ -140,6 +482,8 @@ add_entry_gp_persistent_filespace_node(PG_FUNCTION_ARGS)
     int16 m_dbid;
     char *m_path;
     ItemPointerData persistentTid;
+    PersistentFileSysObjName fsObjName;
+    int64 persistentSerialNum;
 
     filespace = PG_GETARG_OID(0);
     p_dbid = PG_GETARG_INT16(1);
@@ -148,6 +492,10 @@ add_entry_gp_persistent_filespace_node(PG_FUNCTION_ARGS)
     m_path = TextDatumGetCString(PG_GETARG_DATUM(4));
 
     WRITE_PERSISTENT_STATE_ORDERED_LOCK_DECLARE;
+
+    PersistentFilespace_ShmemInit();
+    PersistentFilespace_VerifyInitScan();
+    PersistentFileSysObjName_SetFilespaceDir(&fsObjName, filespace);
 
 	WRITE_PERSISTENT_STATE_ORDERED_LOCK;
     LWLockAcquire(FilespaceHashLock, LW_SHARED);
@@ -167,8 +515,13 @@ add_entry_gp_persistent_filespace_node(PG_FUNCTION_ARGS)
                                     fde->locationBlankPadded2,
                                     m_path);
     ItemPointerCopy(&fde->persistentTid, &persistentTid);
+    persistentSerialNum = fde->persistentSerialNum;
 
     LWLockRelease(FilespaceHashLock);
+
+    fix_heap(&fsObjName, &persistentTid, persistentSerialNum,
+            p_dbid, m_dbid, p_path, m_path, false);
+
     WRITE_PERSISTENT_STATE_ORDERED_UNLOCK;
 
 	PG_RETURN_BOOL(true);
